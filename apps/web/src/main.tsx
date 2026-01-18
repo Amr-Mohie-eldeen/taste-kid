b343ef5dd3550262f7b7d148268320a95292c1a2
Amr-Mohie-eldeen <amr.mohie@hotmail.com>
2026-01-18 13:11:28 +0000

    fix(web): call oidcEarlyInit before app bootstrap

--- apps/web/src/main.tsx
+++ apps/web/src/main.tsx
@@ -1,13 +1,20 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
+import { oidcEarlyInit } from "oidc-spa";
 import App from "./App";
 import "./index.css";
 import { bootstrapOidc } from "./lib/oidc";
 
-bootstrapOidc();
+const { shouldLoadApp } = oidcEarlyInit({
+  BASE_URL: window.location.origin,
+});
 
-ReactDOM.createRoot(document.getElementById("root")!).render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
-);
+if (shouldLoadApp) {
+  bootstrapOidc();
+
+  ReactDOM.createRoot(document.getElementById("root")!).render(
+    <React.StrictMode>
+      <App />
+    </React.StrictMode>
+  );
+}
